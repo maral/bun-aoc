4a8fba14a8c2ae18d13862391c2da78164c6aa88
Marek Lisy <lisy@shoptet.cz>
2023-12-04 22:34:15 +0100

    refactoring because why not

--- src/04/04.ts
+++ src/04/04.ts
@@ -1,34 +1,27 @@
 export function parse(input: string) {
-  return input
+  return input.split('\n').map((line, i) => {
+    const [winning, draw] = line.split(':')[1]!.split('|')
+    const winningCards = lineToCards(winning!)
+    return lineToCards(draw!).filter(card => winningCards.includes(card)).length
+  })
 }
 
 export function partOne(input: ReturnType<typeof parse>): number {
   let sum = 0
-  input.split('\n').forEach((line, i) => {
-    const [winning, draw] = line.split(':')[1]!.split('|')
-    const winningCards = lineToCards(winning!)
-    const numberOfWinningCards = lineToCards(draw!).filter(card =>
-      winningCards.includes(card)
-    ).length
+  input.forEach(numberOfWinningCards => {
     sum += numberOfWinningCards > 0 ? Math.pow(2, numberOfWinningCards - 1) : 0
   })
   return sum
 }
 
 export function partTwo(input: ReturnType<typeof parse>): number {
-  const copies = Array(input.split('\n').length).fill(1)
-  input.split('\n').forEach((line, i) => {
-    const [winning, draw] = line.split(':')[1]!.split('|')
-    const winningCards = lineToCards(winning!)
-    const numberOfWinningCards = lineToCards(draw!).filter(card =>
-      winningCards.includes(card)
-    ).length
-
+  const copies = Array(input.length).fill(1)
+  input.forEach((numberOfWinningCards, i) => {
     for (let j = 0; j < numberOfWinningCards; j++) {
-      copies[i + j + 1] += copies[i];
+      copies[i + j + 1] += copies[i]
     }
   })
-  return copies.reduce((a, b) => a + b, 0)
+  return copies.reduce((prev, curr) => prev + curr, 0)
 }
 
 function lineToCards(line: string): number[] {
