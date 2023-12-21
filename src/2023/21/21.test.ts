57b3e349d266b9675b756f95fd5ebdab4b54b574
Marek Lisy <lisy@shoptet.cz>
2023-12-21 16:30:31 +0100

    finished day 21 part 2

--- src/2023/21/21.test.ts
+++ src/2023/21/21.test.ts
@@ -1,34 +1,58 @@
 import { describe, expect, test } from 'bun:test'
-import { getDiagonalSum, getOptionsForInfiniteFarm, getStraightSum, parse, partOne, partTwo } from './21'
+import {
+  getDiagonalSum,
+  getOptionsForInfiniteFarm,
+  getStraightSum,
+  parse,
+  partOne,
+  partTwo
+} from './21'
 
 const { default: example } = await import('./example.txt')
 
 describe('Day 21', () => {
   describe('Utils', () => {
-    // test('getDiagonalSum', () => {
-    //   expect(getDiagonalSum(1, 1, 1)).toBe(1)
-    //   expect(getDiagonalSum(2, 1, 1)).toBe(3)
-    //   expect(getDiagonalSum(3, 1, 1)).toBe(6)
-    //   expect(getDiagonalSum(4, 1, 1)).toBe(10)
-      
-    //   expect(getDiagonalSum(2, 2, 3)).toBe(2 + 2*3)
-    //   expect(getDiagonalSum(3, 2, 3)).toBe(2 + 3*2 + 2*3)
-    // })
+    test('getDiagonalSum', () => {
+      expect(getDiagonalSum(1, 1, 1, 'odd')).toBe(1)
+      expect(getDiagonalSum(2, 1, 1, 'odd')).toBe(3)
+      expect(getDiagonalSum(3, 1, 1, 'odd')).toBe(6)
+      expect(getDiagonalSum(4, 1, 1, 'odd')).toBe(10)
 
-    // test('getStraightSum', () => {
-    //   expect(getStraightSum(1, 1, 1)).toBe(1)
-    //   expect(getStraightSum(2, 1, 1)).toBe(2)
-    //   expect(getStraightSum(3, 1, 1)).toBe(3)
-    //   expect(getStraightSum(20, 1, 1)).toBe(20)
+      expect(getDiagonalSum(2, 2, 3, 'odd')).toBe(2 + 2 * 3)
+      expect(getDiagonalSum(3, 2, 3, 'odd')).toBe(2 + 3 * 2 + 2 * 3)
+    })
 
-    //   expect(getStraightSum(20, 1, 10)).toBe(110)
-    //   expect(getStraightSum(20, 10, 10)).toBe(200)
-    //   expect(getStraightSum(21, 1, 10)).toBe(111)
-    // })
+    test('getStraightSum', () => {
+      expect(getStraightSum(1, 1, 1, 'odd')).toBe(1)
+      expect(getStraightSum(2, 1, 1, 'odd')).toBe(2)
+      expect(getStraightSum(3, 1, 1, 'odd')).toBe(3)
+      expect(getStraightSum(20, 1, 1, 'odd')).toBe(20)
+
+      expect(getStraightSum(20, 1, 10, 'odd')).toBe(110)
+      expect(getStraightSum(20, 10, 10, 'odd')).toBe(200)
+      expect(getStraightSum(21, 1, 10, 'odd')).toBe(111)
+    })
 
     test('getOptionsForInfiniteFarm', () => {
-      expect(getOptionsForInfiniteFarm(parse(example), 17)).toBe(332)
-      expect(getOptionsForInfiniteFarm(parse(example), 20)).toBe(400)
+      expect(getOptionsForInfiniteFarm(parse(example), 17)).toBe(324)
+      expect(getOptionsForInfiniteFarm(parse(example), 20)).toBe(441)
+    })
+
+    test('getOptionsForInfiniteFarm - mini example', () => {
+      const miniExample = `...
+.S.
+...`
+      expect(getOptionsForInfiniteFarm(parse(miniExample), 5)).toBe(36)
+      expect(getOptionsForInfiniteFarm(parse(miniExample), 7)).toBe(64)
+    })
+
+    test('getOptionsForInfiniteFarm - some obstacles', () => {
+      const example2 = `.....
+.#.#.
+.....
+.#.#.
+.....`
+      expect(getOptionsForInfiniteFarm(parse(example2), 17)).toBe(260)
     })
   })
 })
--- src/2023/21/21.ts
+++ src/2023/21/21.ts
@@ -1,5 +1,4 @@
 import { Queue } from 'js-sdsl'
-import exp from 'node:constants'
 
 const offsets: [number, number][] = [
   [-1, 0],
@@ -41,71 +40,98 @@ export function getOptionsForInfiniteFarm(
   // from start to an edge
   const middle = (input.length - 1) / 2
   const size = input.length
-  const steps = Math.floor((maxSteps - size - 1) / size)
-  const stepsToStraight = Math.floor((maxSteps - middle - 1) / size)
-  console.log(steps, stepsToStraight)
-  const oddSum = stepsThroughMaze(input, middle, middle, maxSteps, 'odd')
-  const evenSum = stepsThroughMaze(input, middle, middle, maxSteps, 'even')
-  console.log(`odd: ${oddSum} even: ${evenSum}`)
-  const oddFields = steps + ((steps + 1) % 2)
-  const evenFields = (steps + 1) + ((steps + 1) % 2)
+  const start = maxSteps % 2 === 0 ? 'even' : 'odd'
+  const startSum = stepsThroughMaze(input, middle, middle, maxSteps, start)
 
   const stepSums = mapOffsets
     .map(([dx, dy]) => {
       const x = dx === -1 ? input[0]!.length - 1 : dx === 1 ? 0 : middle
       const y = dy === -1 ? input.length - 1 : dy === 1 ? 0 : middle
+      const oddSum = stepsThroughMaze(input, x, y, maxSteps, 'odd')
+      const evenSum = stepsThroughMaze(input, x, y, maxSteps, 'even')
 
       let result = 0
       if (isDiagonal(dx, dy)) {
         const initialSteps = size + 1
-        const steps = Math.floor((maxSteps - initialSteps) / size)
-        const remainder = (maxSteps - initialSteps) % size
-        const remainderCount = steps + 1
+        const steps = Math.floor((maxSteps - initialSteps - size + 1) / size)
+        const firstRemainder = maxSteps - (initialSteps + steps * size)
+        const firstRemainderCount = steps + 1
 
-        console.log(
-          `diagonal remainder: ${remainder} remainderCount: ${remainderCount}`
-        )
-        const oddity = (maxSteps - remainder) % 2 === 0 ? 'odd' : 'even'
+        const secondRemainder = firstRemainder - size
+        const secondRemainderCount =
+          firstRemainder >= size ? firstRemainderCount + 1 : 0
 
         result =
-          remainderCount * stepsThroughMaze(input, x, y, remainder, oddity)
+          getDiagonalSum(steps, oddSum, evenSum, start) +
+          firstRemainderCount *
+            stepsThroughMaze(
+              input,
+              x,
+              y,
+              firstRemainder,
+              firstRemainder % 2 === 0 ? 'even' : 'odd'
+            ) +
+          secondRemainderCount *
+            stepsThroughMaze(
+              input,
+              x,
+              y,
+              secondRemainder,
+              secondRemainder % 2 === 0 ? 'even' : 'odd'
+            )
       } else {
         const initialSteps = middle + 1
-        const remainder = (maxSteps - initialSteps) % size
+        const steps = Math.floor((maxSteps - initialSteps) / size)
+        const remainder = maxSteps - (initialSteps + steps * size)
 
-        console.log(`straight remainder: ${remainder}`)
-
-        const oddity = (maxSteps - remainder) % 2 === 0 ? 'odd' : 'even'
-        result = stepsThroughMaze(input, x, y, remainder, oddity)
+        result =
+          getStraightSum(
+            steps,
+            oddSum,
+            evenSum,
+            (middle + 1) % 2 === 0 ? start : opposite(start)
+          ) +
+          stepsThroughMaze(
+            input,
+            x,
+            y,
+            remainder,
+            remainder % 2 === 0 ? 'even' : 'odd'
+          )
       }
-      console.log(result)
       return result
     })
     .reduce((sum, x) => sum + x, 0)
-  console.log(steps, oddSum, evenSum, oddFields, evenFields, stepSums)
-  const straightDiff =
-    stepsToStraight === steps + 1
-      ? 0
-      : -4 * (stepsToStraight % 2 === 0 ? evenSum : oddSum)
+  return startSum + stepSums
+}
 
-  const odd = oddSum * oddFields ** 2
-  const even = evenSum * evenFields ** 2
+function opposite(x: 'odd' | 'even') {
+  return x === 'odd' ? 'even' : 'odd'
+}
+
+export function getStraightSum(
+  steps: number,
+  odd: number,
+  even: number,
+  first: 'odd' | 'even'
+) {
   return (
-    oddSum * oddFields ** 2 +
-    evenSum * evenFields ** 2 +
-    stepSums +
-    straightDiff
+    Math.floor(steps / 2) * (odd + even) +
+    (steps % 2 === 1 ? (first === 'odd' ? odd : even) : 0)
   )
 }
 
-export function getStraightSum(steps: number, odd: number, even: number) {
-  return Math.floor(steps / 2) * (odd + even) + (steps % 2 === 0 ? 0 : odd)
-}
-
-export function getDiagonalSum(steps: number, odd: number, even: number) {
-  const odds = Math.ceil(steps / 2)
-  const evens = Math.floor(steps / 2)
-  return odds * odds * odd + evens * (evens + 1) * even
+export function getDiagonalSum(
+  steps: number,
+  odd: number,
+  even: number,
+  start: 'odd' | 'even'
+) {
+  const inner = Math.ceil(steps / 2) ** 2
+  const outer = Math.floor(steps / 2) * (Math.floor(steps / 2) + 1)
+  const odds = start === 'odd' ? inner : outer
+  const evens = start === 'odd' ? outer : inner
+  return odds * odd + evens * even
 }
 
 function isDiagonal(x: number, y: number) {
