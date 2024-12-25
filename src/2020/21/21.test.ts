ecd6457ea7dd901b27d9687655954b3dedd07f4d
Marek Lisy <lisy@shoptet.cz>
2024-12-25 12:48:56 +0100

    fix 2020/21 tests

--- src/2020/21/21.test.ts
+++ src/2020/21/21.test.ts
@@ -6,13 +6,13 @@ const { default: example } = await import('./example.txt')
 describe('Day 2020/21', () => {
   describe('Part One', () => {
     test('test input 2020/21a', () => {
-      expect(partOne(parse(example))).toBe(0)
+      expect(partOne(parse(example))).toBe(5)
     })
   })
   
   describe('Part Two', () => {
     test('test input 2020/21b', () => {
-      expect(partTwo(parse(example))).toBe(0)
+      expect(partTwo(parse(example))).toBe('mxmxvkd,sqjhc,fvjkl')
     })
   })
 })
